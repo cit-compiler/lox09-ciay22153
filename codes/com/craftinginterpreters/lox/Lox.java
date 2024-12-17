bc1c51cb18bdcaaca64ffdc9d31d3b20ff4209a8
ciay22153 <ciay22153@g.nihon-u.ac.jp>
2024-12-17 11:09:57 +0900

    add breakfast01.bnf

---
+++ codes/com/craftinginterpreters/lox/Lox.java
@@ -0,0 +1,18 @@
+<breakfast>  ::= <protein with breakfast on the side> #
+<breakfast>  ::= <protein with breakfast on the side> #
+<breakfast>  ::= <protein> #
+<breakfast>  ::= <bread> #
+
+<protein>    ::= <crispiness crispy bacon>
+<protein>  ::= <cooked eggs>
+
+<crispiness> ::= <really>
+<crispiness> ::= <really crispiness>
+
+<cooked>     ::= <scrambled>
+<cooked>     ::= <poached>
+<cooked>     ::= <fried>
+
+<bread>      ::= <toast>
+<bread>      ::= <biscuit>
+<bread>      ::= <English muffin> 
